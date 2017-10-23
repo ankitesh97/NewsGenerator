29fbadd0088d9ff85c8e06411d6e13bca553b2cd
Ankitesh Gupta <ankiteshguptas@gmail.com>
2017-10-23 23:09:55 +0530

    completed forward prop for gru

--- Gru.py
+++ Gru.py
@@ -9,8 +9,8 @@ import multiprocessing
 import sys
 
 p_file = open('params.json','r')
-params = json.loads(p_file.read())["gru"]
-
+p = json.loads(p_file.read())
+params = p["gru"]
 
 class GRU:
 
@@ -28,24 +28,69 @@ class GRU:
         self.bg = None
         self.Ug = None # input to gate gate
         self.big = None
+        self.V = None #for the output
+        self.b = None #for output
+        self.hidden_nodes = params["hidden_nodes"]
         self.gate_params_shape = (params["hidden_nodes"],params["hidden_nodes"])
-        self.word_dim = params['preprocess']['vocab_size']
+        self.word_dim = p['preprocess']['vocab_size']
         self.randomizeParams()
+
+
+
+
+    def randomizeParams(self):
+        self.Whz = np.random.randn(self.gate_params_shape[0],self.gate_params_shape[1]) * np.sqrt(1.0/(1+self.hidden_nodes))
+        self.Whr = np.random.randn(self.gate_params_shape[0],self.gate_params_shape[1]) * np.sqrt(1.0/(1+self.hidden_nodes))
+        self.Wg = np.random.randn(self.gate_params_shape[0],self.gate_params_shape[1]) * np.sqrt(1.0/(1+self.hidden_nodes))
+
+        self.Uiz = np.random.randn(self.gate_params_shape[1],self.word_dim) * np.sqrt(1.0/(1+self.word_dim))
+        self.Uir = np.random.randn(self.gate_params_shape[1],self.word_dim) * np.sqrt(1.0/(1+self.word_dim))
+        self.Ug = np.random.randn(self.gate_params_shape[1],self.word_dim) * np.sqrt(1.0/(1+self.word_dim))
+
+        self.bhz = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.hidden_nodes))
+        self.bhr = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.hidden_nodes))
+        self.bg = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.hidden_nodes))
+
+        self.biz = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.word_dim))
+        self.bir = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.word_dim))
+        self.big = np.random.randn(self.hidden_nodes) * np.sqrt(1.0/(1+self.word_dim))
+
+        self.V = np.random.randn(self.word_dim,self.gate_params_shape[1]) * np.sqrt(1.0/self.hidden_nodes+1)
+
+        self.b = np.random.randn(self.word_dim) * np.sqrt(1.0/(1+self.word_dim))
 
 
     # X is the of shape m X time_steps
     # prev_hidden m X hidden_size
     def forward(self, X, prev_hidden, time_step):
-        update_gate = self.updateGate(X, self.Whz, self.Uiz, self.bhz, self.biz, prev_hidden, time_step)
-        
-        pass
+        update_gate = self.updateGate(X, self.Whz, self.Uiz, self.bhz, self.biz, prev_hidden, time_step, self.sigmoid)
+        reset_gate = self.resetGate(X, self.Whr, self.Uir, self.bhr, self.bir, prev_hidden, time_step, self.sigmoid)
+        memory_gate = self.memoryGate(X, self.Wg, self.Ug, self.bg, self.big, reset_gate, prev_hidden, time_step, self.tanh)
+        current_hidden = update_gate * memory_gate + (1-update_gate) * prev_hidden
+        out = self.b + np.dot(current_hidden,self.V.T)
+        output = self.softmax(out)
+        cache = dict(update_gate=update_gate, reset_gate = reset_gate, memory_gate = memory_gate, current_hidden = current_hidden, output=output)
+        return cache
 
     @staticmethod
-    def updateGate(X, W, U , bh, bi, prev_hidden, t):
+    def updateGate(X, W, U , bh, bi, prev_hidden, t, sig):
         from_hidden = np.dot(prev_hidden, W.T) + bh
         from_input = (U[:,X[:,t]]).T + bi
-        return self.sigmoid(from_hidden + from_input)
+        return sig(from_hidden + from_input)
 
+    @staticmethod
+    def resetGate(X, W, U, bh, bi, prev_hidden, t, sig):
+        from_hidden = np.dot(prev_hidden, W.T) + bh
+        from_input = (U[:,X[:,t]]).T + bi
+        return sig(from_hidden + from_input)
+
+    @staticmethod
+    def memoryGate(X, W, U, bh, bi, reset_gate, prev_hidden, t, tanh):
+
+        inter = reset_gate * prev_hidden
+        from_hidden = np.dot(inter, W.T) + bh
+        from_input = (U[:,X[:,t]]).T + bi
+        return tanh(from_hidden + from_input)
 
 
     @staticmethod
@@ -54,3 +99,28 @@ class GRU:
         l = len(z)
         z -= np.max(z,axis=-1).reshape(l,1)
         return 1.0/(1 + np.exp(-z))
+
+
+    @staticmethod
+    def tanh(z):
+        return np.tanh(z)
+
+
+    @staticmethod
+    def softmax(outputs):
+        l = len(outputs)
+        outputs -= np.max(outputs,axis=-1).reshape(l,1) #for numeric stability
+        expo = np.exp(X)
+        return 1.0*expo/np.sum(expo,axis=-1).reshape(l,1)
+
+
+
+if __name__ == '__main__':
+    obj = preprocess()
+    data = obj.load()
+    X = np.array(list(data.X_train[:])).astype(int)[:10]
+    y = np.array(list(data.y_train[:])).astype(int)[:10]
+    cell = GRU()
+    prev_hidden = np.zeros((X.shape[0],cell.hidden_nodes))
+    cache = cell.forward(X, prev_hidden, 0)
+    print cache
